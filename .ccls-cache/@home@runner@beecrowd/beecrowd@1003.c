77ca2826aa565947d89be4f039871a3bc629cf6d
BernardR0 <145278108+BernardR0@users.noreply.github.com>
2024-08-10 17:12:08 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@beecrowd/beecrowd@1003.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+
+
+int main () {
+   int A, B, SOMA;
+  
+  
+    
+  
+  int deu_certo = scanf("%d", &A);
+  
+  deu_certo = scanf("%d", &B);
+  SOMA = A + B; 
+  printf("SOMA = %d\n", SOMA);
+  
+
+
+  return 0;
+}
---
+++ beecrowd/1001.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int A, B, X;
+
+  
+  int deu_certo = scanf("%d", &A);
+
+ 
+  deu_certo = scanf("%d", &B);
+
+  X = A + B;
+
+  printf("X = %d\n", X);
+
+  return 0;
+}

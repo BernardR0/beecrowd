96b09e9f07e37a1cddd52d57d5ab62ec611ddfe5
BernardR0 <bernardosant6@gmail.com>
2024-11-13 23:48:33 -0300

    exercicio 1930 em c

---
+++ beecrowd/1930.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+   int T1, T2 ,T3 ,T4;
+   int soma=0;
+   scanf("%d %d %d %d", &T1,&T2,&T3,&T4);
+ 
+   soma += ((T1-1)+(T2-1)+(T3-1)+T4);
+
+   printf("%d\n", soma);
+
+
+    return 0;
+}
